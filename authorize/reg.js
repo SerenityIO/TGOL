af578f14ee8c050f39d1109440198e490c368cb0
Mykola Shwayko <kolik2001@gmail.com>
2019-04-12 17:08:43 +0300

    folders+styles

---
+++ authorize/reg.js
@@ -0,0 +1,90 @@
+var DataBase = (JSON.parse(window.localStorage.getItem('DataBase'))) ? JSON.parse(window.localStorage.getItem('DataBase')) : [];
+
+var users = [{
+    email: "admin@admin.com",
+    pass: "admin",
+    isonline: false,
+    settings: [{
+        size: '20x10',
+        arr: []
+    }]
+}];
+
+
+function RegisterUser(event) {
+    event.preventDefault();
+
+    var email = event.target.elements.email.value;
+    var F_pass = event.target.elements.FirstPassword.value;
+    var S_pass = event.target.elements.SecondPassword.value;
+
+    var temp = DataBase.forEach(comp => {
+        if (comp.email === email) {
+            alert("This user already exists");
+            return 1;
+        }
+    });
+
+    if (temp != 1) {
+
+        if (F_pass === S_pass) {
+            users = {
+                email: email,
+                pass: F_pass,
+                isonline: false
+            };
+
+            DataBase.push({
+                email: email,
+                pass: F_pass,
+                isonline: false,
+                settings: [{game}]
+            });
+
+            window.localStorage.setItem('DataBase', JSON.stringify(DataBase));
+            event.target.reset();
+            document.location.href = "./log.html";
+        } else {
+            alert("2");
+        }
+    }
+}
+
+
+
+function Login(event) {
+    event.preventDefault();
+    var i = 0;
+    var email = event.target.elements.email.value;
+    var pass = event.target.elements.Password.value;
+
+
+    if (DataBase && DataBase.length) {
+
+        DataBase.forEach(element => {
+            if (element.email === email && element.pass === pass) {
+                alert("Hello");
+                DataBase[i].isonline = true;
+                window.localStorage.setItem('DataBase', JSON.stringify(DataBase));
+                document.location.href = "../main/field.html";
+            }
+            i++;
+        });
+    } else {
+        alert("No user");
+    }
+}
+
+
+function Exit() {
+    var i = 0;
+
+    DataBase.forEach(element => {
+        if (element.email && element.pass) {
+            DataBase[i].isonline = false;
+            window.localStorage.setItem('DataBase', JSON.stringify(DataBase));
+        }
+        i++;
+    });
+    document.location.href = "./log.html";
+} 0
---
+++ main/kvadrat.js
@@ -0,0 +1,150 @@
+var field = document.getElementById('field');
+var squareinf = [];
+
+var arrLS = JSON.parse(window.localStorage.getItem('squareinf'));
+
+var game = {
+	size: '20x10',
+	arr: [],
+	arr2: []
+};
+
+
+function handleSquare(event) {
+
+	var id = event.target.id;
+
+	if (id != 'field') {
+		if (event.target.style.backgroundColor === "") {
+			event.target.style.backgroundColor = "blue";
+			game.arr[id].selected = true;
+		}
+		else {
+			event.target.style.backgroundColor = "";
+			game.arr[id].selected = false;
+		}
+	}
+}
+
+window.onload = function () {
+	redirect();
+	render();
+};
+
+function redirect() {
+
+	var temp = JSON.parse(localStorage.getItem('DataBase'));
+	temp.forEach(element => {
+		if (element.isonline === true) {
+			//alert("Welcome back," + element.email);
+		}
+		else {
+			document.location.href = "../authorize/log.html";
+		}
+	});
+}
+
+function render() {
+	// ToDo: Тут тільки реальне використовування тільки для першого разу. 
+	// ToDo: Ця функція має обновляти поля на основі даних в масиві
+
+	var size = game.size.split('x');
+	var height = +size[1], width = +size[0];
+
+	for (var i = 0; i < width; i++) {
+		for (var j = 0; j < height; j++) {
+			createUserSegment(i + j);
+			// if (game.arr[i + j].selected === true) {
+			// 	document.getElementById(i + j).style.backgroundColor = 'blue';
+			// }
+			// else
+			game.arr.push({ selected: false });
+		}
+	}
+}
+
+function createUserSegment(sum) {
+	var div = document.createElement("div");
+	div.className = 'square';
+	div.id = sum;
+	document.getElementById("field").appendChild(div);
+}
+
+function clearField() {
+	// Done: Тут потрібно видаляти поля і в масиві game.arr 
+	while (field.lastChild) {
+		field.removeChild(field.lastChild);
+		game.arr.shift();
+	}
+}
+
+function Clear() {
+
+	clearField();
+	render();
+}
+function fieldSize() {
+	if (game.size === "20x10") {
+		var temp = document.getElementById('field');
+		temp.style.height = '150px';
+		temp.style.width = '300px';
+	}
+	if (game.size === "50x30") {
+		var temp = document.getElementById("field");
+		temp.style.width = '750px';
+		temp.style.height = '450px';
+	}
+	if (game.size === "70x50") {
+		var temp = document.getElementById('field');
+		temp.style.height = '750px';
+		temp.style.width = '1050px';
+	}
+}
+
+function handleChangeSize(event) {
+	game.size = event.target.value;
+	clearField(game.size);
+	fieldSize(game.size);
+	render();
+}
+
+function randomGen() {
+	// Я преписав функцію ген. Слідкуй за прикладом кодінгу
+	// ToDo: перенеси робону з DOM в render()
+	var field;
+
+	for (var i = 0; i < game.arr.length; i++) {
+		field = document.getElementById(i);
+		if (Math.floor(Math.random() * 4) === 1) {
+			field.style.backgroundColor = "blue";
+			game.arr[i].selected = true;
+		} else {
+			field.style.backgroundColor = "white";
+			game.arr[i].selected = false;
+		}
+	}
+}
+
+
+function play() {
+	var size = game.size.split('x');
+	var height = +size[1], width = +size[0];
+
+	// ToDo: Тут надто багато хардкоду. Подивись алгоритм в тасці 
+
+	for (var i = width; i !== 0; i--) {
+		for (var j = height; j !== 0; j++) {
+			var neighboors = 0;
+			if (i > 0) if (game.arr[i - 1][j]) neighboors++;
+			if (i > 0 && j > 0) if (game.arr[i - 1][j + 1]) neighboors++;
+			if (i > 0 && j < height - 1) if (game.arr[i - 1][j + 1]) neighboors++;
+			if (j < height - 1) if (game.arr[i][j + 1]) neighboors++;
+			if (j > 0) if (game.arr[i][j - 1]) neighboors++;
+			if (i < width - 1) if (game.arr[i + 1][j]) neighboors++;
+			if (i < width - 1 && j > 0) if (game.arr[i + 1][j - 1]) neighboors++;
+			if (i < width - 1 && height - 1) if (game.arr[i + 1][j + 1]) neighboors++;
+			if (game.arr[i][j] && (neighboors < 2 || neighboors > 3)) game.arr2[i][j] = false;
+			if (!game.arr[i][j] && neighboors === 3) game.arr2[i][j] = true;
+		}
+	}
+}
