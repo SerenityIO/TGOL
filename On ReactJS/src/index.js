8724b74ee9eccf073e5b84de2fe24b2ccdb3ccd7
Mykola Shwayko <kolik2001@gmail.com>
2019-06-08 08:26:22 +0300

    Auth and-design

--- On ReactJS/src/index.js
+++ On ReactJS/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/mainStyles.css";
 import Main from './App/mainPage/MainPage.jsx';
+import Auth1 from './App/authPage/auth.jsx';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import 'antd/dist/antd.css';
 
 
-
-ReactDOM.render(
-    <Main />, document.getElementById('root')
-);
+ReactDOM.render((
+    <Router>
+        <Route path='/' component={Auth1} />
+        <Route path='/field' component={Main} />
+    </Router>
+), document.getElementById('root'));
