d0a2ccf17f34a21999b2e7c9952832e878d6e72e
Mykola Shwayko <kolik2001@gmail.com>
2019-06-12 19:28:36 +0300

    local storage user

--- On ReactJS/src/index.js
+++ On ReactJS/src/index.js
@@ -5,11 +5,14 @@ import Main from './App/mainPage/MainPage.jsx';
 import Auth1 from './App/authPage/auth.jsx';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'antd/dist/antd.css';
-
+import RegistrationForm1 from './App/authPage/reg.jsx';
 
 ReactDOM.render((
-    <Router>
-        <Route path='/' component={Auth1} />
-        <Route path='/field' component={Main} />
-    </Router>
+    
+        <Router >
+            <Route path='/' component={RegistrationForm1} />
+            <Route path='/authorize' component={Auth1} />
+            <Route path='/field' component={Main} />
+        </Router >
+    
 ), document.getElementById('root'));
